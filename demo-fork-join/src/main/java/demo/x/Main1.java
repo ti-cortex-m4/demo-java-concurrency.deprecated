0b4a390d9bda03e7774b672a76c6c6b6314a607e
aliakh <liakh.aliaksandr@gmail.com>
2019-04-19 15:40:24 +0300

    Update

--- demo-fork-join/src/main/java/demo/x/Main1.java
+++ demo-fork-join/src/main/java/demo/x/Main1.java
@@ -5,8 +5,8 @@ import java.util.concurrent.ForkJoinPool;
 public class Main1 {
 
     public static void main(String[] args) {
-        FibonacciRecursiveAction task = new FibonacciRecursiveAction(50);
+        StringUpperAction task = new StringUpperAction(2, "abcdefghIjklmnopqrstuvwxyz");
         new ForkJoinPool().invoke(task);
-        System.out.println(task.getNumber());
+        System.out.println(task.getResult());
     }
 }
