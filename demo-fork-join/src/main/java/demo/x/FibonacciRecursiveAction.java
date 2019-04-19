afa197b1a85ee4030dc5769473a93c1bf72db44b
aliakh <liakh.aliaksandr@gmail.com>
2019-04-19 15:30:13 +0300

    Update

---
+++ demo-fork-join/src/main/java/demo/x/FibonacciRecursiveAction.java
@@ -0,0 +1,38 @@
+package demo.x;
+
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveAction;
+
+public class FibonacciRecursiveAction extends RecursiveAction {
+
+    private static final long THRESHOLD = 10;
+
+    private volatile long number;
+
+    public FibonacciRecursiveAction(long number) {
+        this.number = number;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    @Override
+    protected void compute() {
+        long n = number;
+        if (n <= THRESHOLD) {
+            number = fib(n);
+        } else {
+            FibonacciRecursiveAction task1 = new FibonacciRecursiveAction(n - 1);
+            FibonacciRecursiveAction task2 = new FibonacciRecursiveAction(n - 2);
+            ForkJoinTask.invokeAll(task1, task2);
+            number = task1.number + task2.number;
+        }
+    }
+
+    private static long fib(long n) {
+        if (n <= 1) return n;
+        else return fib(n - 1) + fib(n - 2);
+    }
+
+}
---
+++ demo-fork-join/src/main/java/demo/x/Main1.java
@@ -0,0 +1,12 @@
+package demo.x;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        FibonacciRecursiveAction task = new FibonacciRecursiveAction(50);
+        new ForkJoinPool().invoke(task);
+        System.out.println(task.getNumber());
+    }
+}
