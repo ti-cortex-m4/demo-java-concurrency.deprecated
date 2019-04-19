5922b9cda1ce5e04cbb9a06cd18849441d72078f
aliakh <liakh.aliaksandr@gmail.com>
2019-04-19 17:52:03 +0300

    Update

--- demo-fork-join/src/main/java/demo/x/StringUpperCaseAction.java
+++ demo-fork-join/src/main/java/demo/x/StringUpperCaseAction.java
@@ -11,7 +11,7 @@ public class StringUpperCaseAction extends RecursiveAction {
     private String result = "";
 
     public StringUpperCaseAction(int threshold, String value) {
-        System.out.println("Create - (" + value + ") - " + Thread.currentThread().getName());
+        log("Create", value);
         this.threshold = threshold;
         this.value = value;
     }
@@ -24,13 +24,17 @@ public class StringUpperCaseAction extends RecursiveAction {
     protected void compute() {
         if (value.length() > threshold) {
             StringUpperCaseAction task1 = new StringUpperCaseAction(threshold, value.substring(0, value.length() / 2));
-            StringUpperCaseAction task2 = new StringUpperCaseAction(threshold,value.substring(value.length() / 2));
+            StringUpperCaseAction task2 = new StringUpperCaseAction(threshold, value.substring(value.length() / 2));
             ForkJoinTask.invokeAll(task1, task2);
             result = task1.getResult() + task2.getResult();
-            System.out.println("Result - (" + result + ") - " + Thread.currentThread().getName());
+            log("Result", result);
         } else {
             result = value.toUpperCase();
-            System.out.println("Direct - (" + result + ") - " + Thread.currentThread().getName());
+            log("Direct", result);
         }
+    }
+
+    private void log(String s, String result) {
+        System.out.println(String.format("%35s %10s %s", Thread.currentThread().getName(), s , result ));
     }
 }
