99f8eb49d2be1440a2bd7110fa9b1e994fcc5697
aliakh <liakh.aliaksandr@gmail.com>
2019-04-22 12:42:02 +0300

    Update

--- demo-fork-join/src/main/java/demo/ready/FibonacciTask.java
+++ demo-fork-join/src/main/java/demo/ready/FibonacciTask.java
@@ -1,5 +1,9 @@
 package demo.ready;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
 class FibonacciTask extends RecursiveTask<Integer> {
@@ -18,5 +22,20 @@ class FibonacciTask extends RecursiveTask<Integer> {
         f1.fork();
         FibonacciTask f2 = new FibonacciTask(n - 2);
         return f2.compute() + f1.join();
+    }
+
+    private static long fibonacciRecursive(long n) {
+        if (n <= 1) return n;
+        else return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 20;
+
+        System.out.println(fibonacciRecursive(n));
+
+        ForkJoinPool fjp = ForkJoinPool.commonPool();
+        FibonacciTask task = new FibonacciTask(n);
+        System.out.println(fjp.invoke(task));
     }
 }
