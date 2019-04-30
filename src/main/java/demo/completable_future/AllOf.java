4f981f929c56d90aacd57eb2f40dcda4bbbe809c
aliakh <liakh.aliaksandr@gmail.com>
2019-04-30 14:58:29 +0300

    Update

---
+++ src/main/java/demo/completable_future/AllOf.java
@@ -0,0 +1,28 @@
+package demo.completable_future;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class AllOf {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> "Hello");
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> "New");
+        CompletableFuture<String> future3 = CompletableFuture.supplyAsync(() -> "World");
+
+        CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(future1, future2, future3);
+        combinedFuture.get();
+
+        System.out.println(future1.isDone());
+        System.out.println(future2.isDone());
+        System.out.println(future3.isDone());
+
+        String combined = Stream.of(future1, future2, future3)
+                .map(CompletableFuture::join)
+                .collect(Collectors.joining(" "));
+
+        System.out.println(combined);
+    }
+}
